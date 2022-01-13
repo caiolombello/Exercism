d0fd76e7d9cc80981975cd999bc84a1bc4622e71
caiol <caiolvbarbieri@pm.me>
2022-01-13 10:05:55 -0300

    solved

--- java/blackjack/src/main/java/Blackjack.java
+++ java/blackjack/src/main/java/Blackjack.java
@@ -1,19 +1,51 @@
 public class Blackjack {
 
     public int parseCard(String card) {
-        throw new UnsupportedOperationException("Please implement the Blackjack.parseCard method");
+        switch (card) {
+            case "ace":
+                return 11;
+            case "two":
+                return 2;
+            case "three":
+                return 3;
+            case "four":
+                return 4;
+            case "five":
+                return 5;
+            case "six":
+                return 6;
+            case "seven":
+                return 7;
+            case "eight":
+                return 8;
+            case "nine":
+                return 9;
+            case "ten": case "jack": case "queen": case "king":
+                return 10;
+            default:
+                return 0;
+        }
     }
 
     public boolean isBlackjack(String card1, String card2) {
-        throw new UnsupportedOperationException("Please implement the Blackjack.isBlackjack method");
+        return parseCard(card1) + parseCard(card2) == 21;
     }
 
     public String largeHand(boolean isBlackjack, int dealerScore) {
-        throw new UnsupportedOperationException("Please implement the Blackjack.largeHand method");
+        if (isBlackjack && !(dealerScore >= 10))
+            return "W";
+        else if (isBlackjack && dealerScore == 11)
+            return "S";
+        else if (dealerScore == 11)
+            return "P";
+        return "S";
     }
 
     public String smallHand(int handScore, int dealerScore) {
-        throw new UnsupportedOperationException("Please implement the Blackjack.smallHand method");
+        if ((handScore >= 12 && !(dealerScore >= 7)) || handScore >= 17)
+            return "S";
+        else
+            return "H";
     }
 
     // FirstTurn returns the semi-optimal decision for the first turn, given the cards of the player and the dealer.
