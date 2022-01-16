5366497b73345acb0770d82d9b346fa75f20848c
caiol <caiolvbarbieri@pm.me>
2022-01-16 17:58:38 -0300

    solved

--- java/cars-assemble/src/main/java/CarsAssemble.java
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -1,17 +1,24 @@
 public class CarsAssemble {
+    private static final int NUMBER_IN_HOURS = 221;
+
+    public double speeds (int speed) {
+        if (speed == 10)
+            return 0.77;
+        else if (speed == 9)
+            return 0.80;
+        else if (speed >= 5)
+            return 0.90;
+        else if (speed < 5)
+            return 1.0;
+        else
+            return speed;
+    }
 
     public double productionRatePerHour(int speed) {
-        if (speed == 10)
-            return (speed * 221) * 0.77;
-        else if (speed == 9)
-            return (speed * 221) * 0.80;
-        else if (speed >= 5)
-            return (speed * 221) * 0.90;
-        else
-            return speed * 221;
+        return NUMBER_IN_HOURS * speed * speeds(speed);
     }
 
     public int workingItemsPerMinute(int speed) {
-        return (int) (productionRatePerHour(speed) / 60);
+        return (int) productionRatePerHour (speed) / 60;
     }
 }
