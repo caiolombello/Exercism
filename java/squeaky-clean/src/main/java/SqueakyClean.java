9c405fda43ff7510254a7335bdbfb26adb69b468
caiol <caiolvbarbieri@pm.me>
2022-01-16 17:33:05 -0300

    solved

--- java/squeaky-clean/src/main/java/SqueakyClean.java
+++ java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,6 +1,25 @@
 class SqueakyClean {
     static String clean(String identifier) {
-        boolean isWhitespace;
+        var stringBuilder = new StringBuilder();
+        var isAfterDash = false;
+        for (var c : identifier.toCharArray()) {
+            if (isGreekLowercase(c)) {
+                continue;
+            } else if (isAfterDash) {
+                stringBuilder.append(Character.toUpperCase(c));
+            } else if (Character.isWhitespace(c)) {
+                stringBuilder.append('_');
+            } else if (Character.isISOControl(c)) {
+                stringBuilder.append("CTRL");
+            } else if (Character.isLetter(c)) {
+                stringBuilder.append(c);
+            }
+            isAfterDash = (c == '-');
+        }
+        return stringBuilder.toString();
+    }
 
+    private static boolean isGreekLowercase(char c) {
+        return (c >= 'α' && c <= 'ω');
     }
 }
