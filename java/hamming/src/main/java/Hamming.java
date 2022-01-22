e76bf9e16e627ceb122eba4ec32e0c9333d07524
caiolombello <caiolvbarbieri@pm.me>
2022-01-22 18:51:22 -0300

    solved

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,9 +1,30 @@
-public class Hamming {
-    public Hamming(String leftStrand, String rightStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+import java.util.stream.IntStream;
+
+class Hamming {
+    private final String leftStrand;
+    private final String rightStrand;
+
+    Hamming(String leftStrand, String rightStrand) {
+        if (leftStrand.length() > rightStrand.length() && rightStrand.length() != 0) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+        if (leftStrand.length() < rightStrand.length() && leftStrand.length() != 0) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+        if (leftStrand.isEmpty() && rightStrand.length() != 0) {
+            throw new IllegalArgumentException("left strand must not be empty.");
+        }
+        if (rightStrand.isEmpty() && leftStrand.length() != 0) {
+            throw new IllegalArgumentException("right strand must not be empty.");
+        }
+        this.leftStrand = leftStrand;
+        this.rightStrand = rightStrand;
     }
 
-    public int getHammingDistance() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    long     getHammingDistance() {
+        return IntStream
+                .range(0, leftStrand.length())
+                .filter(i -> leftStrand.charAt(i) != rightStrand.charAt(i))
+                .count();
     }
 }
